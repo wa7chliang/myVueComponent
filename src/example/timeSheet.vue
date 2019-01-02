3ca5cabdff749650f4ef73c9ff1c6826141fa4fc
Nolin-Lin <673295625@qq.com>
2019-01-02 22:13:05 +0800

    add timeSheet

---
+++ src/example/timeSheet.vue
@@ -0,0 +1,22 @@
+<template>
+  <timeSheet></timeSheet>
+</template>
+
+<script>
+import timeSheet from '@/components/timeSheet/index'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      datas: {
+      }
+    }
+  },
+  components: {
+    timeSheet
+  },
+  methods: {
+  }
+}
+</script>
